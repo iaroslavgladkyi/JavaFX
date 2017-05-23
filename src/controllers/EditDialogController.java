d5d88248ecbb6280981b0b8935b57d6576817fa8
iaroslavgladkyi <igladkyi@gmail.com>
2017-05-23 12:19:38 +0300

    add the @FXML anotations for components of project

--- src/controllers/EditDialogController.java
+++ src/controllers/EditDialogController.java
@@ -1,4 +1,22 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class EditDialogController {
+
+    @FXML
+    private Button btnOk;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private TextField txtFIO;
+
+    @FXML
+    private TextField txtPhone;
+
+
 }
