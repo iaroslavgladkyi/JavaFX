c75a9762eee4f3720db4b5378a15d8901f783efa
iaroslavgladkyi <igladkyi@gmail.com>
2017-05-23 12:43:20 +0300

    create interface AddressBook + create class Person

---
+++ src/interfaces/AddressBook.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+import objects.Person;
+
+/**
+ * Created by gaffs on 23/05/17.
+ */
+public interface AddressBook {
+
+    void add(Person person);
+
+    void update(Person person);
+
+    void delete(Person person);
+    
+}
---
+++ src/objects/Person.java
@@ -0,0 +1,27 @@
+package objects;
+
+/**
+ * Created by gaffs on 23/05/17.
+ */
+public class Person {
+
+    private String fio;
+    private String phone;
+
+    public String getFio() {
+        return fio;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
